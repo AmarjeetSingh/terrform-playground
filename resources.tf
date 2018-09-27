78dfbb7a88d6450a61563f9acc34854854a51d6d
Amarjeet Singh <amarjeet.singh@tomtom.com>
2018-09-27 15:39:50 +0530

    added gile

--- resources.tf
+++ resources.tf
@@ -1,8 +1,34 @@
 resource "aws_vpc" "environment-example-amar" {
-  cidr_block = "10.22.22.0/24"
+  cidr_block = "10.22.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
   tags {
     Name = "terraform-aws-example-amar"
+  }
+}
+
+resource "aws_subnet" "amar-subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-amar.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-amar.id}"
+  availability_zone = "ap-south-1"
+}
+
+resource "aws_subnet" "amar-subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-amar.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-amar.id}"
+  availability_zone = "ap-south-1"
+}
+
+resource "aws_security_group" "amar-subnet-security" {
+  vpc_id = "${aws_vpc.environment-example-amar.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-amar.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
   }
 }
